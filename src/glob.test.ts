8420b3e43f39426155419d57e4a77b3faec19e6f
Viljami <v.kuosmanen@epilot.cloud>
2023-03-20 18:57:46 +0100

    0.4.0

--- src/glob.test.ts
+++ src/glob.test.ts
@@ -1,53 +1,53 @@
-import { globPaths } from "./glob";
+import { globValues, globPaths } from "./glob";
 
 describe("globValues", () => {
   it("should return matched values in an object by path", () => {
-    expect(globPaths("a", { a: 1, b: true, c: "3", d: [1, 2], f: { a: "b" } })).toEqual([1]);
-    expect(globPaths("b", { a: 1, b: true, c: "3", d: [1, 2], f: { a: "b" } })).toEqual([true]);
-    expect(globPaths("c", { a: 1, b: true, c: "3", d: [1, 2], f: { a: "b" } })).toEqual(["3"]);
-    expect(globPaths("d", { a: 1, b: true, c: "3", d: [1, 2], f: { a: "b" } })).toEqual([[1, 2]]);
-    expect(globPaths("f", { a: 1, b: true, c: "3", d: [1, 2], f: { a: "b" } })).toEqual([{ a: "b" }]);
+    expect(globValues("a", { a: 1, b: true, c: "3", d: [1, 2], f: { a: "b" } })).toEqual([1]);
+    expect(globValues("b", { a: 1, b: true, c: "3", d: [1, 2], f: { a: "b" } })).toEqual([true]);
+    expect(globValues("c", { a: 1, b: true, c: "3", d: [1, 2], f: { a: "b" } })).toEqual(["3"]);
+    expect(globValues("d", { a: 1, b: true, c: "3", d: [1, 2], f: { a: "b" } })).toEqual([[1, 2]]);
+    expect(globValues("f", { a: 1, b: true, c: "3", d: [1, 2], f: { a: "b" } })).toEqual([{ a: "b" }]);
   });
 
   it("should return matched values in an array by index", () => {
-    expect(globPaths("0", [1, true, "3", [1, 2], { a: "b" }])).toEqual([1]);
-    expect(globPaths("1", [1, true, "3", [1, 2], { a: "b" }])).toEqual([true]);
-    expect(globPaths("2", [1, true, "3", [1, 2], { a: "b" }])).toEqual(["3"]);
-    expect(globPaths("3", [1, true, "3", [1, 2], { a: "b" }])).toEqual([[1, 2]]);
-    expect(globPaths("4", [1, true, "3", [1, 2], { a: "b" }])).toEqual([{ a: "b" }]);
+    expect(globValues("0", [1, true, "3", [1, 2], { a: "b" }])).toEqual([1]);
+    expect(globValues("1", [1, true, "3", [1, 2], { a: "b" }])).toEqual([true]);
+    expect(globValues("2", [1, true, "3", [1, 2], { a: "b" }])).toEqual(["3"]);
+    expect(globValues("3", [1, true, "3", [1, 2], { a: "b" }])).toEqual([[1, 2]]);
+    expect(globValues("4", [1, true, "3", [1, 2], { a: "b" }])).toEqual([{ a: "b" }]);
   });
 
   it("should return nested values by path", () => {
-    expect(globPaths("a", { a: "bc" })).toEqual(["bc"]);
-    expect(globPaths("a.b", { a: { b: 1 }, c: 2 })).toEqual([1]);
-    expect(globPaths("a.b.c", { a: { b: { c: "deeply nested" } }, c: 2 })).toEqual(["deeply nested"]);
+    expect(globValues("a", { a: "bc" })).toEqual(["bc"]);
+    expect(globValues("a.b", { a: { b: 1 }, c: 2 })).toEqual([1]);
+    expect(globValues("a.b.c", { a: { b: { c: "deeply nested" } }, c: 2 })).toEqual(["deeply nested"]);
   });
 
   it("should return matched object properties with wildcard", () => {
-    expect(globPaths("*", { a: 1, b: 2, c: 3 })).toEqual([1, 2, 3]);
+    expect(globValues("*", { a: 1, b: 2, c: 3 })).toEqual([1, 2, 3]);
   });
 
   it("should return matched array items with wildcard", () => {
-    expect(globPaths("*", [1, 2, 3])).toEqual([1, 2, 3]);
+    expect(globValues("*", [1, 2, 3])).toEqual([1, 2, 3]);
   });
 
   it("should return nested values with wildcard", () => {
-    expect(globPaths("a.*.d", { a: { b: { d: 1, f: 2 }, c: { d: 3, f: 4 } } })).toEqual([1, 3]);
-    expect(globPaths("a.*.f", { a: { b: { d: 1, f: 2 }, c: { d: 3, f: 4 } } })).toEqual([2, 4]);
-    expect(globPaths("a.b.*", { a: { b: { d: 1, f: 2 }, c: { d: 3, f: 4 } } })).toEqual([1, 2]);
-    expect(globPaths("a.c.*", { a: { b: { d: 1, f: 2 }, c: { d: 3, f: 4 } } })).toEqual([3, 4]);
+    expect(globValues("a.*.d", { a: { b: { d: 1, f: 2 }, c: { d: 3, f: 4 } } })).toEqual([1, 3]);
+    expect(globValues("a.*.f", { a: { b: { d: 1, f: 2 }, c: { d: 3, f: 4 } } })).toEqual([2, 4]);
+    expect(globValues("a.b.*", { a: { b: { d: 1, f: 2 }, c: { d: 3, f: 4 } } })).toEqual([1, 2]);
+    expect(globValues("a.c.*", { a: { b: { d: 1, f: 2 }, c: { d: 3, f: 4 } } })).toEqual([3, 4]);
   });
 
   it("should return deeply nested values with double wildcard", () => {
-    expect(globPaths("**.d", { a: { b: { d: 1, f: 2 }, c: { d: 3, f: 4 } } })).toEqual([1, 3]);
-    expect(globPaths("**.f", { a: { b: { d: 1, f: 2 }, c: { d: 3, f: 4 } } })).toEqual([2, 4]);
-    expect(globPaths("a.**.d", { a: { b: { d: 1, f: 2 }, c: { d: 3, f: { d: 4 } } } })).toEqual([1, 3, 4]);
-    expect(globPaths("z.**.f", { a: { b: { d: 1, f: 2 }, c: { d: 3, f: { d: 4 } } } })).toEqual([]);
+    expect(globValues("**.d", { a: { b: { d: 1, f: 2 }, c: { d: 3, f: 4 } } })).toEqual([1, 3]);
+    expect(globValues("**.f", { a: { b: { d: 1, f: 2 }, c: { d: 3, f: 4 } } })).toEqual([2, 4]);
+    expect(globValues("a.**.d", { a: { b: { d: 1, f: 2 }, c: { d: 3, f: { d: 4 } } } })).toEqual([1, 3, 4]);
+    expect(globValues("z.**.f", { a: { b: { d: 1, f: 2 }, c: { d: 3, f: { d: 4 } } } })).toEqual([]);
   });
 
   it("should return values using wildcard attributes", () => {
-    expect(globPaths("a*", { ab: 7, ac: 8, ba: 9, bc: 10 })).toEqual([7, 8]);
-    expect(globPaths("b?", { ab: 7, ac: 8, ba: 9, bc: 10 })).toEqual([9, 10]);
+    expect(globValues("a*", { ab: 7, ac: 8, ba: 9, bc: 10 })).toEqual([7, 8]);
+    expect(globValues("b?", { ab: 7, ac: 8, ba: 9, bc: 10 })).toEqual([9, 10]);
   });
 });
 
