cbac53609a075cb8127f596e360adcdeeb9f7a5b
Viljami <v.kuosmanen@epilot.cloud>
2023-03-20 18:36:03 +0100

    Prettier

--- src/globValues.ts
+++ src/globValues.ts
@@ -1,6 +1,5 @@
 import { globToRegExp } from "./deno-glob";
 
-
 export function globValues(globPattern: string, obj: any): any[] {
   const matcher = globToRegExp(globPattern, { globstar: true });
   const globByDepth = new Map();
@@ -14,19 +13,24 @@ export function globValues(globPattern: string, obj: any): any[] {
       if (matcher.test(currentPath.join("."))) {
         values.push(value);
       } else if (typeof value === "object") {
-        if (globPattern.includes('**')) {
+        if (globPattern.includes("**")) {
           // if the glob pattern contains **, we need to traverse all the way down
           traverse(value, currentPath);
           continue;
-        } 
+        }
 
         // don't traverse if the path doesn't match partially
         let partialMatcher = globByDepth.get(path.length);
         if (!partialMatcher) {
-          partialMatcher = globToRegExp(globPattern.split('.').slice(0, path.length + 1).join('.'));
+          partialMatcher = globToRegExp(
+            globPattern
+              .split(".")
+              .slice(0, path.length + 1)
+              .join(".")
+          );
           globByDepth.set(path.length, partialMatcher);
         }
-        const isPartialMatch = partialMatcher.test(currentPath.join('.'));
+        const isPartialMatch = partialMatcher.test(currentPath.join("."));
 
         if (isPartialMatch) {
           traverse(value, currentPath);
